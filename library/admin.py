2f25fa6c9d55f31dd2256f5d464280a51b66630d
Enrico Gandini <enricogandini93@gmail.com>
2022-07-03 11:26:44 +0200

    Create Admin sites for Author, Publisher, and Book
    The site for Book was personalized, and is now sortable by publication date and title.

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author, Book, Publisher
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "author",
+        "date_publication",
+        "publisher",
+    )
+    sortable_by = ("title", "date_publication")
+
+
+admin.site.register(Author)
+admin.site.register(Publisher)
