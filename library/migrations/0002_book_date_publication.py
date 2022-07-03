36b1e095e3b3e755c30f3a33c0d4a2c8a7310a63
Enrico Gandini <enricogandini93@gmail.com>
2022-07-03 11:49:25 +0200

    Format migrations with black and isort

--- library/migrations/0002_book_date_publication.py
+++ library/migrations/0002_book_date_publication.py
@@ -6,13 +6,17 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('library', '0001_initial'),
+        ("library", "0001_initial"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='book',
-            name='date_publication',
-            field=models.DateField(blank=True, help_text='The publication date of a book. If only the year is available, by default use the 1st of January of that year.', null=True),
+            model_name="book",
+            name="date_publication",
+            field=models.DateField(
+                blank=True,
+                help_text="The publication date of a book. If only the year is available, by default use the 1st of January of that year.",
+                null=True,
+            ),
         ),
     ]
