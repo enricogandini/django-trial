91115d4b3d954eff17b88a37807977858ddf13f0
Enrico Gandini <enricogandini93@gmail.com>
2022-07-07 19:39:36 +0200

    Change date display format to only year

--- main/settings.py
+++ main/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
 from pathlib import Path
+
+from django.conf.locale.en import formats as en_formats
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -113,6 +114,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+en_formats.DATE_FORMAT = "Y"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
